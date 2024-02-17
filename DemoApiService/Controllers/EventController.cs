d9a73754d3a005fc0d20c769e5715746c36569f5
Johan Smarius <jsmarius@hotmail.com>
2024-02-17 13:52:47 +0100

    Add project files.

---
+++ DemoApiService/Controllers/EventController.cs
@@ -0,0 +1,33 @@
+﻿using DemoApiService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApiService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Event>> GetEvents()
+        { 
+            // Create a demo list of 10 events 
+            var events = new List<Event>(); 
+            for (int i = 0; i < 10; i++)
+            {
+                events.Add(new Event
+                {
+                    Id = i,
+                    Name = $"Event {i}",
+                    Description = $"Description for Event {i}",
+                    StartTime = DateTime.Now.AddDays(i),
+                    EndTime = DateTime.Now.AddDays(i + 1)
+                });
+            }
+
+            return Ok(events);
+
+        }
+
+    }
+}
---
+++ DemoApiService/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿namespace DemoApiService.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string Location { get; set; }
+    }
+}
